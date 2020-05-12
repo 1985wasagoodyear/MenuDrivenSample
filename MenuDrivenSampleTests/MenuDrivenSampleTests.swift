ec87d142a4ae23982316f1b3412c221d64a5f39e
1985wasagoodyear <1985wasagoodyear>
2020-05-12 14:21:18 -0400

    Initial Commit

---
+++ MenuDrivenSampleTests/MenuDrivenSampleTests.swift
@@ -0,0 +1,36 @@
+//
+//  MenuDrivenSampleTests.swift
+//  Created 5/12/20
+//  Using Swift 5.0
+// 
+//  Copyright © 2020 yu. All rights reserved.
+//
+//  https://github.com/1985wasagoodyear
+//
+
+import XCTest
+@testable import MenuDrivenSample
+
+class MenuDrivenSampleTests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
